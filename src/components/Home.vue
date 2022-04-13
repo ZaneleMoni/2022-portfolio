2826cef494117bdcf513b162e2819c23f4bf6148
Ntombizanele <zanelemoni4@gmail.com>
2022-04-14 00:20:34 +0200

    Add files via upload

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,45 @@
 <template>
-  <section class="home py-5" id='home'>
+  <section class="home py-5" id="home">
     <div class="container-lg">
       <div class="row min-vh-100 align-items-center align-content-center">
         <div class="col-md-6 mt-5 mt-md-0">
           <div class="home-img text-center">
-            <img src="https://i.postimg.cc/Kjt2QpKG/zanele2.jpg" class='rounded-circle mw-100 bg-danger' alt="profile-img">
+            <img
+              src="https://i.postimg.cc/Kjt2QpKG/zanele2.jpg"
+              class="rounded-circle mw-100 bg-danger"
+              alt="profile-img"
+            />
           </div>
         </div>
         <div class="col-md-6 mt-5 mt-md-0 order-md-first">
           <div class="home-text">
-            <p class='text-muted mb-1'>Hi there! I am a</p>
-            <h1 class='text-danger text-uppercase fs-1 fw-bold'>Full stack developer </h1>
-            <h2 class='fs-4'>Ntombizanele</h2>
-            <a href="#projects" class='btn btn-danger px-3 mt-3'>My work</a>
+            <p class="text-muted mb-1">Hi there! I am a</p>
+            <h1 class="text-danger text-uppercase fs-1 fw-bold">
+              Full stack developer
+            </h1>
+            <h2 class="fs-4">Ntombizanele Moni</h2>
+            <a
+              href="https://codepen.io/ZaneleMoni"
+              class="btn btn-danger px-3 mt-3"
+              target="_blank"
+              >My work</a
+            >
           </div>
         </div>
       </div>
     </div>
   </section>
-    <div class="container">
-    <hr>
+  <div class="container">
+    <hr />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-img{
+img {
   height: auto;
   width: 550px;
   object-fit: cover;
