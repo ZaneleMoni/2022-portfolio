3bff5d58310d2907f520da2eddde4e73d53145ca
Ntombizanele <zanelemoni4@gmail.com>
2022-04-16 00:08:59 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm fixed-top">
     <div class="container-lg">
-      <a class="navbar-brand text-danger fw-bold fs-3" href="#">>NM</a>
+      <a class="navbar-brand text-danger fw-bold fs-3" href="#">NM</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
